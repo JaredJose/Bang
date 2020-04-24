166a3153600593c88b6bff9874b3ca88314c9710
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2020-04-23 19:26:03 -0700

    user class created in model folder

---
+++ src/Model/User.java
@@ -0,0 +1,31 @@
+public class User
+{
+	private String username;
+	private String password;
+
+	public User(String uname, string passwd)
+	{
+		this.username = uname;
+		this.password = passwd;
+	}
+
+	public void setUsername(String name)
+	{
+		this.username = name;
+	}
+
+	public void setPassword(String passwd)
+	{
+		this.password = passwd;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+}
