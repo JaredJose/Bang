b6db9a676e78c490d745bd63288e7ff1b9cd35a5
JaredJose <42820125+JaredJose@users.noreply.github.com>
2020-05-26 16:57:30 -0700

    Created Memory Class

--- src/Control/Driver.java
+++ src/Control/Driver.java
@@ -1,5 +1,11 @@
 package Control;
 
-public class Driver {
+import java.util.ArrayList;
 
+import Model.Memory;
+
+public class Driver {
+	ArrayList<Memory> MemoryList = new ArrayList<Memory>();
+	
+	
 }
--- src/Model/Memory.java
+++ src/Model/Memory.java
@@ -1,5 +1,16 @@
 package Model;
 
+import java.io.File;
+import java.time.LocalDate;
+
 public class Memory {
+	File pt;
+	LocalDate time;
+	String caption;
 	
+	public Memory(File pt, LocalDate time, String capt) {
+		this.pt = pt;
+		this.time = time;
+		caption = capt;
+	}
 }
