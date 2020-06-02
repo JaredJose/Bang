965cd1deb81d6d8ae878a4f62b5e9c326b792bfd
courtneydavies <courtneydavies@192.168.7.229>
2020-06-02 12:45:42 -0700

    Added a ton of jars to make aws work. I think that's it right now

--- src/Model/loginDatabaseMethods.java
+++ src/Model/loginDatabaseMethods.java
@@ -1,5 +1,12 @@
 package Model;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
 public class loginDatabaseMethods {
 	//to work with Control login class
 	//primary objective is to isolate the userID based off of user/pass
@@ -9,6 +16,26 @@ public class loginDatabaseMethods {
 	//returns -1 if no user is found with the passed credentials
 	public int getUserID(String user, String pass) {
 		//@Ethan, however you can get the querying to work
+		
+		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (/Users/johnmortensen/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+        	.withCredentials(credentialsProvider)
+            .withRegion("us-west-2")
+            .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("CovidSimData");
+		
 		return -1;
 	}
 }
